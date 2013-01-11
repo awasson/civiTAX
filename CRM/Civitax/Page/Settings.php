a864a6018f7f8b35b3eb880d9722bed2175d2f61
awasson <awasson@lunadesign.org>
2013-01-10 22:52:06 -0800

    stubbing out config page

--- CRM/Civitax/Page/Settings.php
+++ CRM/Civitax/Page/Settings.php
@@ -4,11 +4,45 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Civitax_Page_Settings extends CRM_Core_Page {
   function run() {
-    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-    CRM_Utils_System::setTitle(ts('Settings'));
-
-    // Example: Assign a variable for use in a template
-    $this->assign('currentTime', date('Y-m-d H:i:s'));
+  	
+  	
+  	
+	/**
+  	 * GET APPLICABLE CONTRIBUTION TYPES  
+  	 * FROM THE civicrm_contribution_type TABLE
+  	 * PUT RESULTS IN AN ARRAY AND SEND TO THE VIEW 
+  	 */
+  	$sql = 'SELECT * FROM civicrm_contribution_type';
+  	$dao = CRM_Core_DAO::executeQuery($sql);
+  	$arr_contribution_types = array();
+    $x = 0;
+    while( $dao->fetch( ) ) {   
+       	$arr_contribution_types[$x]['id'] = $dao->id;
+       	$arr_contribution_types[$x]['name'] = $dao->name;
+       	$x++;	
+    }
+  	$this->assign('arr_contribution_types', $arr_contribution_types);
+  	
+  	
+  	/**
+  	 * GET TAX RATES FROM THE civi_tax_type TABLE  
+  	 * PUT RESULTS IN AN ARRAY AND SEND TO THE VIEW 
+  	 */
+  	$sql = 'SELECT * FROM civi_tax_type';
+  	$dao = CRM_Core_DAO::executeQuery($sql);
+  	$arr_arr_tax_types = array();
+    $x = 0;
+    while( $dao->fetch( ) ) {   
+       	$arr_tax_types[$x]['id'] = $dao->id;
+       	$arr_tax_types[$x]['tax'] = $dao->tax;
+       	$arr_tax_types[$x]['rate'] = $dao->rate;
+       	$x++;	
+    }
+  	$this->assign('arr_tax_types', $arr_tax_types);
+  
+  
+    // Set the page-title 
+    CRM_Utils_System::setTitle(ts('CiviTax Settings'));
 
     parent::run();
   }
