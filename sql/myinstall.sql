289f128f45ecb6e2ae0c222588ce61cd344dde70
awasson <awasson@lunadesign.org>
2013-02-09 10:04:48 -0800

    repair typo in sql

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `civi_tax_type` (
   UNIQUE KEY `id_UNIQUE` (`id` ASC)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `civicrm`.`civi_tax_invoicing` (
+CREATE TABLE IF NOT EXISTS `civi_tax_invoicing` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `invoice_id` VARCHAR(255) NOT NULL DEFAULT 0 ,
   `pre_tax` FLOAT NOT NULL DEFAULT 0 ,
