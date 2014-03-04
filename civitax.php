965c73a08d6a082479e4db0bde06f89429b5085c
Andrew Wasson <awasson@lunadesign.org>
2014-03-03 17:55:49 -0800

    Update to new DB schema and add tax processing.
    Update to work with new Financial Type database schema in CiviCRM 4.3 +
    
    Added hook to handle calculating, recording and applying tax
    transactions to contributions running through the system.

--- civitax.php
+++ civitax.php
@@ -79,7 +79,7 @@ function civitax_civicrm_buildForm($formName, $form) {
 		case 'CRM_Contribute_Form_Contribution_Main':
 
 			$civitax_values = $form->getVar('_values');
-			$civitax_contribution_type_id = $civitax_values['contribution_type_id'];
+			$civitax_contribution_type_id = $civitax_values['financial_type_id'];
 			
 			$sql = "SELECT civi_tax_type.tax FROM civi_tax_contribution_type INNER JOIN civi_tax_type ON civi_tax_type.id = civi_tax_contribution_type.tax_id WHERE civi_tax_type.active = 1 AND contribution_type_id = " . $civitax_contribution_type_id;
 			$dao = CRM_Core_DAO::executeQuery($sql);
@@ -169,6 +169,101 @@ function civitax_civicrm_buildForm($formName, $form) {
 	
 	}
 
+}
+
+
+/**
+ * Hook implementation for altering payment parameters before talking to a payment processor back end.
+ *
+ * @param string $paymentObj
+ *    instance of payment class of the payment processor invoked (e.g., 'CRM_Core_Payment_Dummy')
+ * @param array &$rawParams
+ *    array of params as passed to to the processor
+ * @params array  &$cookedParams
+ *     params after the processor code has translated them into its own key/value pairs
+ * @return void
+ */
+function civitax_civicrm_alterPaymentProcessorParams($paymentObj, &$rawParams, &$cookedParams) {
+     
+    // TESTING: Throw the $paymentObj into a session to inspect. 
+    // $_SESSION['rawParams'] = $rawParams;
+    $_SESSION['cookedParams'] = $cookedParams;
+    // $_SESSION['paymentObj'] = $paymentObj;
+    
+    // Grab transaction variables from the $rawParams array.
+    $invoice_id = $rawParams['invoiceID'];
+    $pre_tax = $rawParams['amount'];
+    $contributionType_accounting_code = $rawParams['contributionType_accounting_code'];
+    
+    // Grab the financial type ID with query against civicrm_financial_account table. 
+    $sql = "SELECT id FROM civicrm_financial_account WHERE accounting_code = $contributionType_accounting_code"; 
+    $dao = CRM_Core_DAO::executeQuery($sql);
+    $dao->fetch();
+    $financial_type_id = $dao->id;
+    
+    /**
+     * GET TAX ID
+     * LOOP FOR MULTIPLE TAXES 
+     */  
+	$sql = "SELECT * FROM civi_tax_contribution_type WHERE contribution_type_id = $financial_type_id";
+    $dao = CRM_Core_DAO::executeQuery($sql);
+    $arr_contribution_type_ids = array();
+    $x = 0;
+    while( $dao->fetch( ) ) {   
+       	$arr_contribution_type_ids[$x]['id'] = $dao->tax_id;
+       	$x++;	
+    }
+    
+    /**
+     * Check the results 
+     * If we have results from above, it's taxable
+     */
+    if($x > 0 && is_array($arr_contribution_type_ids)) {
+    
+    	//LOOP THROUGH TAXES
+    	$total_tax = 0;
+    	$limit = count($arr_contribution_type_ids);
+    	for($x = 0; $x < $limit; $x++) {
+    		$tax_id = $arr_contribution_type_ids[$x]['id'];
+    		$sql = "SELECT * FROM civi_tax_type WHERE id = " . $tax_id;
+    		$dao = CRM_Core_DAO::executeQuery($sql);
+    		$dao->fetch();    		
+    		$tax_name = $dao->tax;
+    		$tax_rate = ($dao->rate * .01);
+    		$tax_active = $dao->active;
+    		
+    		// IF THE TAX IS ACTIVE...
+    		if($tax_active == 1) {
+    
+    			// Calculate taxes
+    			$tax_charged = $pre_tax * $tax_rate;
+    			$total_tax += $tax_charged;
+    			
+    			// IF THE TRANSACTION HAS MULTIPLE TAXES CARRY post_tax VALUE UNTIL FINAL CALCULATION
+    			if($x < ($limit - 1)) {
+    				$post_tax = "carried";
+    			} else {
+    				$post_tax = $pre_tax + $total_tax;
+    			}
+    			
+    			// Save a record of the transaction information for reporting later.
+    			$sql = "INSERT INTO civi_tax_invoicing(invoice_id, tax_id, tax_name, pre_tax, tax_rate, tax_charged, post_tax) VALUES('".$invoice_id."', ".$tax_id.", '".$tax_name."', '".$pre_tax."', '".$tax_rate."', '".$tax_charged."', '".$post_tax."')";
+    			$dao2 = CRM_Core_DAO::executeQuery($sql);
+    		
+    		}
+    		
+    	
+    	}
+    	
+    	/**
+    	 * Apply the $post_tax value to the $cookedParams array 
+    	 * so that the processor charges the total with tax
+    	 */
+    	 $cookedParams['amount'] = $post_tax;
+    	
+    
+    }
+    
 }
 
 
