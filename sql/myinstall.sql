a0fddf81c7f5282ebbde7c40bd90f7fb2a74acbf
Andrew Wasson <awasson@lunadesign.org>
2014-03-03 17:33:20 -0800

    Update database tables for recording taxed transactions

--- sql/myinstall.sql
+++ sql/myinstall.sql
@@ -7,16 +7,22 @@ CREATE TABLE IF NOT EXISTS `civi_tax_type` (
   UNIQUE KEY `id_UNIQUE` (`id` ASC)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `civi_tax_invoicing` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `invoice_id` VARCHAR(255) NOT NULL DEFAULT 0 ,
-  `pre_tax` FLOAT NOT NULL DEFAULT 0 ,
-  `tax` FLOAT NOT NULL DEFAULT 0 ,
-  `tax_charged` FLOAT NOT NULL DEFAULT 0 ,
-  `post_tax` FLOAT NOT NULL DEFAULT 0 ,
+
+
+CREATE TABLE `civi_tax_invoicing` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `invoice_id` varchar(255) NOT NULL DEFAULT '0',
+  `tax_id` int(11) NOT NULL,
+  `tax_name` varchar(45) NOT NULL,
+  `pre_tax` varchar(7) NOT NULL DEFAULT '0',
+  `tax_rate` varchar(7) NOT NULL DEFAULT '0',
+  `tax_charged` varchar(45) NOT NULL DEFAULT '0',
+  `post_tax` varchar(45) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) 
 ) ENGINE = InnoDB;
+
+
 
 CREATE TABLE IF NOT EXISTS `civi_tax_contribution_type` (
   `tax_id` int(11) NOT NULL,
